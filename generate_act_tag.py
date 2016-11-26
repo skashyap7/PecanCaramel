00a6cd9b112f1fd5b46adfbbd69da879ef85d0f7
rakshith7 <rakshith@usc.edu>
2016-11-25 19:13:36 -0800

    Updated act_tag generator

--- generate_act_tag.py
+++ generate_act_tag.py
@@ -1,14 +1,19 @@
 import nltk
 from collections import defaultdict
-
+"""
+ Class generates act_tag based on the existing corpus in nltk
+ There are total of 15 act tags which can be assigned to any sentence
+"""
 class Act_Tag:
+
+	# Initialize the corpus and train the classifier based on this featureset
 	def __init__(self):
 		self.tagged_data = nltk.corpus.nps_chat.xml_posts()[:10000]
 		self.output = []
 		self.act_set = [(self.__tokenize_sentence(post.text), post.get('class')) for post in self.tagged_data]
 		self.classifier = nltk.NaiveBayesClassifier.train(self.act_set)
 
-		
+	# Method returns the sentence in required format
 	def __tokenize_sentence(self, utterance):
 		word_act = {}
 
@@ -16,6 +21,7 @@ class Act_Tag:
 			word_act['contains({})'.format(word.lower())] = True
 		return word_act
 
+	# Method classifies the given sentence based on trained classifier set
 	def get_act_tag(self, utterance):
 		inp_utt = []
 		for utt in utterance:
